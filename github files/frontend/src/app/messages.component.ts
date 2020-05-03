ca43327a684be96ea9049f605e41437dc1a2de7d
donkeykong91 <darylnator@gmail.com>
2020-05-03 05:04:58 -0700

    added new messages in component

--- github files/frontend/src/app/messages.component.ts
+++ github files/frontend/src/app/messages.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'fe-messages',
-    template: `this is the messages component`
+    template: `this is the messages component <div *ngFor="let message of messages">{{ message.text }} by: {{ message.owner }}</div>`
 })
 export class MessagesComponent {
-
+    messages = [{text: 'some text', owner: 'tim'}, {text: 'other message', owner: 'jane'}];
 }
